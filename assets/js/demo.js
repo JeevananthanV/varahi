6485c2aa4f32dd4d58380debe23473ffbb98ffb3
JeevananthanV <jeevananthanjeeva170902@gmail.com>
2025-09-14 23:37:55 +0530

    Add assets folder with Git LFS

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -1,4 +1,3 @@
-document.querySelector(".menu-item a").addEventListener("click", function (e) {
-  e.preventDefault();
-  console.log("Clicked"); // <- This stops the link from navigating
-});
+version https://git-lfs.github.com/spec/v1
+oid sha256:2831960196eb2c4359dfb37418d459b90502661f1a9270874576509ef1f44b72
+size 174
